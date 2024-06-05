9aad014b23568abf8ca9449095a62b8d15f99a00
Cemre Dağ <130288206+gumaruw@users.noreply.github.com>
2024-06-05 22:35:02 +0300

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+   defaultCommandTimeout: 10000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    viewportWidth: 1280,
+    viewportHeight: 720 // Varsayılan komut zaman aşımını 10 saniye olarak ayarla
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
   },
 });
