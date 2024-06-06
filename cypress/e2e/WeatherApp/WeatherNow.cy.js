b0d3dabac361177c754e5736cacdea09a7498d97
clwope <tahabolat2003@gmail.com>
2024-06-06 14:06:37 +0300

    fixed issues on cypress tests

--- cypress/e2e/WeatherApp/WeatherNow.cy.js
+++ cypress/e2e/WeatherApp/WeatherNow.cy.js
@@ -1,7 +1,6 @@
 describe('WeatherNow Uygulaması', () => {
   it('Uygulama yüklenir ve başlık görüntülenir', () => {
     cy.visit('http://localhost:3000'); // Uygulamayı başlat
-cy.title().should('eq', 'WeatherNow'); // Başlık etiketini kontrol et
-
-  });
+    cy.title().should('eq', 'WeatherNow'); // Başlık etiketini kontrol et
+  });
 });
--- cypress/e2e/WeatherApp/fetchWeather.cy.js
+++ cypress/e2e/WeatherApp/fetchWeather.cy.js
@@ -1,39 +1,48 @@
 describe('WeatherNow Uygulaması', () => {
   beforeEach(() => {
-    cy.intercept('GET', '**/weather*', {
-      fixture: 'weather.json'
-    }).as('getWeather');
+    cy.visit('http://localhost:3000');
+  });
 
-    cy.intercept('GET', '**/forecast*', {
-      fixture: 'forecast.json'
-    }).as('getForecast');
+  it('Geçerli şehir için hava durumu bilgilerini kontrol etme', () => {
+
+    cy.get('.css-6j8wv5-Input').should('exist')
+    cy.log('Input field exists');
+    cy.get('.css-6j8wv5-Input').type('London');
+    cy.wait(2000)
+    cy.log('Typed "London" into input field');
+
+    cy.get('#react-select-3-option-0').click();
+    cy.log('Selected city from dropdown');
+
+    cy.contains('London').should('be.visible');
+    cy.get('.temperature')
+    cy.contains('mist').should('be.visible');
+    cy.contains('18°C').should('be.visible');
   });
 
   it('Geçerli şehir için API\'den hava durumu verisi alma', () => {
-    cy.visit('http://localhost:3000');
 
-    cy.get('.css-6j8wv5-Input').should('exist').then(() => {
-      cy.log('Input field exists');
-      cy.get('.css-6j8wv5-Input').type('London');
-      cy.log('Typed "London" into input field');
+    cy.intercept('GET', '**/weather*').as('getWeather');
+    
+    cy.intercept('GET', '**/forecast*').as('getForecast');
 
-      cy.get('#react-select-3-option-0').click();
-      cy.log('Selected city from dropdown');
+    cy.get('.css-6j8wv5-Input').should('exist')
+    cy.log('Input field exists');
+    cy.get('.css-6j8wv5-Input').type('London');
+    cy.log('Typed "London" into input field');
+    cy.wait(2000)
+    cy.get('#react-select-3-option-2').click();
+    cy.log('Selected city from dropdown');
 
-      // API çağrısının tetiklenip tetiklenmediğini kontrol et
-      cy.wait('@getWeather', { timeout: 10000 }).then((interception) => {
-        cy.log('Weather API call made');
-        expect(interception.response.statusCode).to.eq(200);
-      });
+    // API çağrısının tetiklenip tetiklenmediğini kontrol et
+    cy.wait('@getWeather', { timeout: 2000 }).then((interception) => {
+      cy.log('Weather API call made');
+      expect(interception.response.statusCode).to.eq(200);
+    });
 
-      cy.wait('@getForecast', { timeout: 10000 }).then((interception) => {
-        cy.log('Forecast API call made');
-        expect(interception.response.statusCode).to.eq(200);
-      });
-
-      cy.contains('London').should('be.visible');
-      cy.contains('Partly cloudy').should('be.visible');
-      cy.contains('20°C').should('be.visible');
+    cy.wait('@getForecast', { timeout: 2000 }).then((interception) => {
+      cy.log('Forecast API call made');
+      expect(interception.response.statusCode).to.eq(200);
     });
   });
 });
